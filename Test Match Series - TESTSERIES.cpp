2c7be1f413c3adad54730a36bd8b20b34fe18f18
Sparsh Gupta <sparshgpt18@gmail.com>
2022-07-05 11:36:08 +0530

    Add files via upload

---
+++ Test Match Series - TESTSERIES.cpp
@@ -0,0 +1,81 @@
+// https://www.codechef.com/SNCKQL21/problems/TESTSERIES
+/*
+
+     ░██████╗██████╗░░█████╗░██████╗░░██████╗██╗░░██╗  ░██████╗░██╗░░░██╗██████╗░████████╗░█████╗░
+     ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░██║  ██╔════╝░██║░░░██║██╔══██╗╚══██╔══╝██╔══██╗
+     ╚█████╗░██████╔╝███████║██████╔╝╚█████╗░███████║  ██║░░██╗░██║░░░██║██████╔╝░░░██║░░░███████║
+     ░╚═══██╗██╔═══╝░██╔══██║██╔══██╗░╚═══██╗██╔══██║  ██║░░╚██╗██║░░░██║██╔═══╝░░░░██║░░░██╔══██║
+     ██████╔╝██║░░░░░██║░░██║██║░░██║██████╔╝██║░░██║  ╚██████╔╝╚██████╔╝██║░░░░░░░░██║░░░██║░░██║
+     ╚═════╝░╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝  ░╚═════╝░░╚═════╝░╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝
+    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+    █████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗
+    ╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝
+    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fi          first
+#define se          second
+#define pb          push_back
+#define mp          make_pair
+#define all(x)      x.begin(), x.end()
+#define ll          long long
+#define pll         pair<ll, ll>
+#define vll         vector<long long>
+#define inf         1e18
+#define gcd(a, b)   __gcd(a, b)
+#define range(a,b)  substr(a,b-a+1)
+#define fori(a, n)  for (ll i = a; i < n; i++)
+#define forj(a, n)  for (ll j = a; j < n; j++)
+#define fork(a, n)  for (ll k = a; k < n; k++)
+#define print(x)    for (auto i : x) {cout << i << " ";}
+#define FIO         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+template <class T>
+bool comp(T a, T b) {
+    if (a < b)
+        return true;
+    return false;
+}
+
+int main()
+{
+    FIO;
+
+    #ifndef ONLINE_JUDGE
+    //remove this piece of code when this has to be submitted in kickstart, coding ninjas
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    //freopen is used to associate a file with stdin or stdout stream in C++
+    #endif
+
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll a, b, c, d, e;
+        cin >> a >> b >> c >> d >> e;
+        ll suma = 0, sumb = 0;
+        suma += a == 0;
+        suma += b == 0;
+        suma += c == 0;
+        suma += d == 0;
+        suma += e == 0;
+        sumb += a == 1;
+        sumb += b == 1;
+        sumb += c == 1;
+        sumb += d == 1;
+        sumb += e == 1;
+        if (sumb == 5 - suma - sumb)
+            cout << "DRAW\n";
+        else if (sumb > 5 - suma - sumb)
+            cout << "INDIA\n";
+        else
+            cout << "ENGLAND\n";
+    }
+
+    return 0;
+}
